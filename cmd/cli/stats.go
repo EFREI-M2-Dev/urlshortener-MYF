2e49095a4536b6c0d40ade76905fe3ea333f8aa6
Mat <71132805+mat512@users.noreply.github.com>
2025-06-27 10:46:49 +0200

    feat(cli): implement stats cmd

--- cmd/cli/stats.go
+++ cmd/cli/stats.go
@@ -15,7 +15,7 @@ import (
 )
 
 // TODO : variable shortCodeFlag qui stockera la valeur du flag --code
-
+var shortCodeFlag string
 
 // StatsCmd représente la commande 'stats'
 var StatsCmd = &cobra.Command{
@@ -29,14 +29,25 @@ Exemple:
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO : Valider que le flag --code a été fourni.
 		// os.Exit(1) si erreur
+		if shortCodeFlag == "" {
+            fmt.Fprintln(os.Stderr, "ERREUR: Le flag --code est requis.")
+            os.Exit(1)
+        }
 
 
 		// TODO : Charger la configuration chargée globalement via cmd.cfg
-
+		cfg := cmd2.Cfg
+        if cfg == nil {
+            fmt.Fprintln(os.Stderr, "ERREUR: Impossible de charger la configuration globale.")
+            os.Exit(1)
+        }
 
 		// TODO 3: Initialiser la connexion à la base de données SQLite avec GORM.
 		// log.Fatalf si erreur
-
+		db, err := gorm.Open(sqlite.Open(cfg.Database.Name), &gorm.Config{})
+        if err != nil {
+            log.Fatalf("FATAL: Impossible d'ouvrir la base de données: %v", err)
+        }
 
 
 		sqlDB, err := db.DB()
@@ -49,14 +60,22 @@ Exemple:
 		defer sqlDB.Close()
 
 		// TODO : Initialiser les repositories et services nécessaires NewLinkRepository & NewLinkService
-		linkRepo :=
-		linkService :=
+		linkRepo := repository.NewLinkRepository(db)
+        linkService := services.NewLinkService(linkRepo)
 
 		// TODO 5: Appeler GetLinkStats pour récupérer le lien et ses statistiques.
 		// Attention, la fonction retourne 3 valeurs
 		// Pour l'erreur, utilisez gorm.ErrRecordNotFound
 		// Si erreur, os.Exit(1)
-
+		link, totalClicks, err := linkService.GetLinkStats(shortCodeFlag)
+        if err != nil {
+            if err == gorm.ErrRecordNotFound {
+                fmt.Fprintf(os.Stderr, "Aucun lien trouvé pour le code court: %s\n", shortCodeFlag)
+            } else {
+                fmt.Fprintf(os.Stderr, "Erreur lors de la récupération des statistiques: %v\n", err)
+            }
+            os.Exit(1)
+        }
 
 
 		fmt.Printf("Statistiques pour le code court: %s\n", link.ShortCode)
@@ -69,10 +88,10 @@ Exemple:
 // Il est utilisé pour définir les flags que cette commande accepte.
 func init() {
 	// TODO 7: Définir le flag --code pour la commande stats.
-
+	StatsCmd.Flags().StringVar(&shortCodeFlag, "code", "", "Code court du lien à analyser")
 	// TODO Marquer le flag comme requis
-
+	StatsCmd.MarkFlagRequired("code")
 
 	// TODO : Ajouter la commande à RootCmd
-
+	cmd2.RootCmd.AddCommand(StatsCmd)
 }
